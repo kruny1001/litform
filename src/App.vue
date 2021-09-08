b155bcc6ef885c8d6b8cd00d29da23d7c726af5c
lipjae <dlwognscap@gmail.com>
2021-09-08 14:24:09 +0900

    input form

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,34 @@
 <template lang="pug">
-img(alt="Vue logo" src="./assets/logo.png")
-  //- <img alt="Vue logo" src="./assets/logo.png">
-  //- <HelloWorld msg="Welcome to Your Vue.js App"/>
+section
+  Generator( :model="model" :data="testData")
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Generator from './generator.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
-  }
+    Generator
+  },
+  data() {
+    return {
+      testData: {
+        id: 1,
+        name: 'asd'
+      },
+      model: {
+        type: "input",
+        inputType: "text",
+        label: "텍스트",
+        modelKey: "name",
+        readonly: false,
+        validator: '',
+        placeholder: '텍스트 필드',
+        hintText: '텍스트 필드 입니다.'
+      }
+    }
+  },
 }
 </script>
 
